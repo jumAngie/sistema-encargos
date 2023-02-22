4f47d097ea834f05f5b0a06b99c905241ba9e19b
juanmsagas <angie.camposyc03@gmail.com>
2023-02-22 13:46:38 -0600

    update a usuarios
    Se eliminó el campo Nombre en la tabla Usuarios

--- CAS  ENVIOS.sql
+++ CAS  ENVIOS.sql
@@ -33,7 +33,6 @@ CREATE TABLE tblRolPantallas
 
 CREATE TABLE tbUsuarios(
 	usu_ID				INT IDENTITY(1,1) PRIMARY KEY,
-	usu_Nombre			NVARCHAR(250) NOT NULL,
 	usu_Usuario			NVARCHAR(200) NOT NULL,
 	emp_Id				INT,
 	rol_ID				INT NOT NULL,
