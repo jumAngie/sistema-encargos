5f42ded4eafb97aa221e946e58eb839406f87ef6
jumAngie <angie.camposyc03@gmail.com>
2023-02-24 15:04:37 -0600

    EDITAR Y ELIMINAR

--- UDPS DE EDITAR Y ELIMINAR LAS 12 TABLAS.sql
+++ UDPS DE EDITAR Y ELIMINAR LAS 12 TABLAS.sql
@@ -47,5 +47,172 @@ BEGIN
 			SET		rep_Estado = 0, [rep_UsuarioMod] = @UsuarioMod , 
 					rep_FechaMod = GETDATE()
 END
-
+go
 -------------------------------------------- TABLA CIUDADES ----------------------------------------------
+CREATE OR ALTER PROCEDURE UDP_Editar_Ciudades
+		@ciu_ID				INT, 
+		@ciu_Descripcion	NVARCHAR(200),
+		@ciu_UsuarioMod		VARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbCiudades
+		SET		ciu_Descripcion = @ciu_Descripcion, [ciu_UsuarioMod] = @ciu_UsuarioMod,
+				[ciu_FechaMod] = GETDATE()		
+		WHERE	[ciu_ID] = @ciu_ID
+END
+GO
+-------------------------------------------- TABLA CLIENTES ----------------------------------------------
+CREATE OR ALTER PROCEDURE UDP_Editar_Clientes
+		@client_ID				INT, 
+		@client_Nombre			NVARCHAR(250), 
+		@client_Identidad		NVARCHAR(30), 
+		@client_EstadoCivil		CHAR(1), 
+		@client_Sexo			CHAR(1), 
+		@client_Telefono		NVARCHAR(50), 
+		@client_Saldo			NVARCHAR(100), 
+		@client_LimiteCredito	NVARCHAR(100), 
+		@client_Descuento		NVARCHAR(100),  
+		@client_UsuarioMod		NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbClientes
+		SET		[client_Nombre] = @client_Nombre,				[client_Identidad] = @client_Identidad,
+				[client_EstadoCivil] = @client_EstadoCivil,		[client_Sexo] = @client_Sexo,
+				[client_Telefono] = @client_Telefono,			[client_Saldo] = @client_Saldo,
+				[client_LimiteCredito] = @client_LimiteCredito, [client_Descuento] = @client_Descuento,
+				[client_UsuarioMod] = @client_UsuarioMod
+END
+GO
+CREATE OR ALTER PROCEDURE UDP_Eliminar_Cliente
+		@ID			INT,
+		@UsuarioMod	NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbClientes
+		SET		[client_Estado] = 0, [client_UsuarioMod] = @UsuarioMod, [client_FechaMod] = GETDATE()
+END
+
+GO
+-------------------------------------------- TABLA DEPARTAMENTOS  -------------------------------------------
+CREATE OR ALTER PROCEDURE UDP_Editar_Departamentos
+		@depto_ID				INT, 
+		@depto_Descripcion		NVARCHAR(255),
+		@usu_UsuarioMod			NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbDepartamentos
+		SET		[depto_Descripcion] = @depto_Descripcion, [usu_UsuarioMod] = @usu_UsuarioMod,
+				[depto_FechaMod] = GETDATE()
+		WHERE	[depto_ID] = @depto_ID
+END
+
+-------------------------------------------- TABLA DIRECCIONES  -------------------------------------------
+GO
+CREATE OR ALTER PROCEDURE UDP_Editar_Direcciones
+		@direc_ID				INT, 
+		@direc_ClienteID		INT, 
+		@direc_DireccionExacta	NVARCHAR(MAX), 
+		@direc_CiudadID			INT, 
+		@direc_UsuarioMod		NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbDirecciones
+		SET		[direc_ClienteID] = @direc_ClienteID, [direc_DireccionExacta] = @direc_DireccionExacta,
+				[direc_CiudadID] = @direc_CiudadID, [direc_UsuarioMod] = @direc_UsuarioMod, 
+				[direc_FechaMod] = GETDATE()
+		WHERE	[direc_ID] = @direc_ID
+END
+
+GO
+CREATE OR ALTER PROCEDURE UDP_Eliminar_Direcciones
+			@ID				INT,
+			@UsuarioModi	NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbDirecciones
+		SET		[direc_Estado] = 0, [direc_UsuarioMod] = @UsuarioModi, [direc_FechaMod] = GETDATE()
+END
+go
+-------------------------------------------- TABLA EMPLEADOS  -------------------------------------------
+CREATE OR ALTER PROCEDURE UDP_Editar_Empleados
+		@emp_Id			INT, 
+		@emp_Name		NVARCHAR(150), 
+		@emp_Apellido	NVARCHAR(150), 
+		@emp_DNI		INT, 
+		@emp_FechaNac	DATE, 
+		@ciu_ID			INT, 
+		@est_ID			CHAR(1), 
+		@emp_Sexo		CHAR(1), 
+		@carg_Id		INT, 
+		@emp_UsuModif	NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbEmpleados
+		SET		[emp_Name] = @emp_Name, [emp_Apellido] = @emp_Apellido, [emp_DNI] = @emp_DNI,
+				[emp_FechaNac]= @emp_FechaNac, [ciu_ID] = @ciu_ID, [est_ID] = @est_ID,
+				[emp_Sexo] = @emp_Sexo, [carg_Id] = @carg_Id, [emp_UsuModif] = @emp_UsuModif,
+				[emp_FechaModif] = GETDATE()
+		WHERE	[emp_Id] = @emp_Id
+END
+
+GO
+CREATE OR ALTER PROCEDURE UDP_Eliminar_Empleados
+		@ID			INT,
+		@UsuModi	NVARCHAR(50)
+AS
+BEGIN
+		UPDATE  tbEmpleados
+		SET		[emp_Estado] = 0, [emp_UsuModif] = @UsuModi, [emp_FechaModif] = GETDATE()
+		WHERE	[emp_Id] = @ID
+END
+go
+---------------------------------------- TABLA ESTADOS CIVILES  ------------------------------------------
+CREATE OR ALTER PROCEDURE	UDP_Editar_EstadosCiviles
+		@est_ID				CHAR(1), 
+		@est_Descripcion	NVARCHAR(100), 
+		@est_UsuMod			NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbEstadosCiviles
+		SET		[est_Descripcion] = @est_Descripcion, [est_UsuMod] = @est_UsuMod,
+				[est_FechaMod] = GETDATE()
+		WHERE	[est_ID] = @est_ID
+END
+
+go
+CREATE OR ALTER PROCEDURE	UDP_Eliminar_EstadosCiviles
+		@ID			CHAR(1),
+		@UsuMod		NVARCHAR(50)
+AS
+BEGIN
+		UPDATE tbEstadosCiviles
+		SET		[est_Estado] = 0, [est_UsuMod] = @UsuMod, [est_FechaMod] = GETDATE()
+		WHERE	[est_ID] = @ID
+END
+
+GO
+---------------------------------------- TABLA FABRICAS  ------------------------------------------
+CREATE OR  ALTER PROCEDURE UDP_Editar_Fabricas
+		@fab_ID				INT, 
+		@fab_Descripcion	NVARCHAR(200), 
+		@fab_Telefono		NVARCHAR(250), 
+		@fab_UsuarioMod		NVARCHAR(50)
+		
+AS
+BEGIN
+		UPDATE	tbFabricas
+		SET		[fab_Descripcion] = @fab_Descripcion,	[fab_Telefono] = @fab_Telefono,
+				[fab_UsuarioMod] = @fab_UsuarioMod,		[fab_FechaMod] = GETDATE()
+		WHERE	[fab_ID] = @fab_ID
+END
+
+go
+CREATE OR ALTER PROCEDURE UDP_Eliminar_Fabricas
+		@ID INT,
+		@UsuMod NVARCHAR(50)
+AS
+BEGIN
+		UPDATE	tbFabricas
+		SET		[fab_Estado] = 0, [fab_UsuarioMod] = @UsuMod, fab_FechaCreacion = GETDATE()
+		WHERE	[fab_ID] = @ID
+END
