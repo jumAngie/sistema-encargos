8f58ee27bbcf8921971dca09de8f99ae5fa3981f
Quinta707 <saraieqp30@gmail.com>
2023-03-01 14:39:30 -0600

    modal eliminar articulos

---
+++ Sistema Envios/Scripts/EliminarArticulo.js
@@ -0,0 +1,22 @@
+﻿function Borrar() {
+    var id = $(this).data('id');
+    $.ajax({
+        url: "/Articulos/Delete",
+        method: "POST",
+        data: { id: id },
+        success: function (data) {
+            window.location.reload();
+        }
+    })
+}
+
+$(function () {
+    $('.btn-delete').click(function () {
+        var id = $(this).data('id');
+        $('#btn-confirm-delete').attr('href', '/Articulos/Delete/' + id);
+        $('#confirm-delete-modal').modal('show');
+    });
+    $('#btn-close-modal').click(function () {
+        $('#confirm-delete-modal').modal('hide');
+    });
+});
