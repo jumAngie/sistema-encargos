459f1711ce38aedc58859923495ce7eca1d318ef
Angie Campos <angie.camposyc03@gmail.com>
2023-03-02 21:40:50 -0600

    A

--- Sistema Envios/Models/Empleados.cs
+++ Sistema Envios/Models/Empleados.cs
@@ -18,35 +18,35 @@ namespace Sistema_Envios.Models
         public int emp_Id { get; set; }
 
         [Display(Name = "Nombres")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public string emp_Name { get; set; }
 
         [Display(Name = "Apellidos")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public string emp_Apellido { get; set; }
 
         [Display(Name = "DNI")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public string emp_DNI { get; set; }
 
         [Display(Name = "Fecha de Nacimiento")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public Nullable<System.DateTime> emp_FechaNac { get; set; }
 
         [Display(Name = "Ciudad")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public int ciu_ID { get; set; }
 
         [Display(Name = "Estado Civil")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public string est_ID { get; set; }
 
         [Display(Name = "Sexo")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public string emp_Sexo { get; set; }
 
         [Display(Name = "Cargo")]
-        [Required(ErrorMessage = "Este campo {0} es requerido.")]
+        [Required(ErrorMessage = "*")]
         public int carg_Id { get; set; }
     }
 }
