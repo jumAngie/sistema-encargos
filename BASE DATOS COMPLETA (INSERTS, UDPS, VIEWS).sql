e73068e914958ae5369817f8b473498e8577178a
Angie Campos <angie.camposyc03@gmail.com>
2023-03-05 13:17:17 -0600

    Triggers para EMPLEADO - USUARIO

--- BASE DATOS COMPLETA (INSERTS, UDPS, VIEWS).sql
+++ BASE DATOS COMPLETA (INSERTS, UDPS, VIEWS).sql
@@ -1834,6 +1834,67 @@ BEGIN
 	WHERE		art_ID = (Select art_ID from inserted)
 END;
 GO
+
+--- TRIGGERS PARA ELIMINAR, CLIENTES - PEDIDOS - PEDIDOS DETALLES
+--							EMPLEADOS - USUARIOS
+go
+CREATE OR ALTER TRIGGER tg_EliminarCliente 
+ON tbClientes
+AFTER UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF UPDATE(client_Estado)
+	BEGIN
+		
+		UPDATE	tbPedidos
+		SET		pedi_Estado = 0, [pedi_UsuarioMod] = (SELECT [client_UsuarioMod] FROM inserted),  [pedi_FechaMod]= GETDATE()
+		WHERE	pedi_DireccionID = (SELECT direc_ID FROM tbDirecciones WHERE direc_ClienteID = 
+				(SELECT client_ID FROM inserted))
+		AND		(SELECT client_Estado from inserted) = 0
+	END
+END
+
+go
+CREATE OR ALTER TRIGGER tg_EliminarDetalles 
+ON tbPedidos
+FOR UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF UPDATE([pedi_Estado])
+	BEGIN
+		UPDATE	tbPedidoDetalles
+		SET		[det_Estado] = 0, 
+				[det_UsuModif] = (select distinct [pedi_UsuarioMod] FROM inserted), 
+				[det_FechaModif] = GETDATE()
+		FROM	tbPedidoDetalles
+		INNER JOIN inserted ON tbPedidoDetalles.pedi_ID = inserted.pedi_ID
+		WHERE	tbPedidoDetalles.pedi_ID IN (SELECT pedi_ID FROM inserted)
+		AND		(SELECT pedi_Estado FROM inserted) = 0
+	END
+END
+
+GO
+CREATE OR ALTER TRIGGER tg_EliminarUsuario 
+ON [dbo].[tbEmpleados]
+AFTER UPDATE
+AS
+BEGIN
+	SET NOCOUNT ON;
+	IF UPDATE([emp_Estado])
+	BEGIN
+		UPDATE	tbUsuarios
+		SET		usu_Estado = 0, 
+				usu_UsuarioMod = inserted.emp_UsuModif, 
+				usu_FechaMod = GETDATE()
+		FROM	tbUsuarios
+		INNER JOIN inserted ON tbUsuarios.emp_Id = inserted.emp_Id
+		WHERE	tbUsuarios.emp_Id IN (SELECT emp_Id FROM inserted)
+		AND		inserted.emp_Estado = 0
+	END
+END
+GO
 -- UDPS PARA LISTAR --
 CREATE OR ALTER PROCEDURE UDP_ListaDeArticulosPorPedido
 		@ID	INT
